1032cb8178d19a2d264f1b0b98ea8bd587304b2b
sakaschekolda <ashelkunov1904@gmail.com>
2024-09-17 20:36:30 +0400

    first commit

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import "fmt"
+
+//task_1
+func main() {
+
+	var num int
+
+	fmt.Print("\nEnter the value: ")
+
+	fmt.Scan(&num)
+
+	fmt.Print("\n1st task\n")
+	if num%2 == 0 {
+		fmt.Print("even")
+	} else {
+		fmt.Print("odd")
+	}
+	WhatIsNum(num)
+	Foreach()
+}
+
+//task_2
+func WhatIsNum(a int) {
+	fmt.Print("\n\n2nd task\n")
+	if a > 0 {
+		fmt.Print("Positive")
+	} else if a == 0 {
+		fmt.Print("Zero")
+	} else {
+		fmt.Print("Negative")
+	}
+}
+
+//task_3
+func Foreach() {
+
+	var myString string
+
+	fmt.Print("\n\n3rd task\n")
+	for i := 1; i <= 10; i++ {
+		fmt.Print(i, " ")
+	}
+
+	fmt.Print("\n\n4th task\nEnter text: ")
+	fmt.Scan(&myString)
+	StringNum(myString)
+}
+
+//task_4
+func StringNum(a string) {
+	fmt.Print("lenght ", "'", a, "'", " is - ", len(a))
+	ForRect()
+}
+
+//task_5
+type Rectangle struct {
+	a float64
+	b float64
+}
+
+func ForRect() {
+	fmt.Print("\n\n5th task\n", "enter two sides of rectangle:\n")
+
+	var a1 float64
+	var a2 float64
+
+	fmt.Scan(&a1, &a2)
+
+	rectangle := Rectangle{a: a1, b: a2}
+	fmt.Print("rectangle area is - ", rectangle.a*rectangle.b)
+
+	MultNum(int(a1), int(a2))
+}
+
+//task_6
+func MultNum(a int, b int) {
+
+	averageValue := float64((float64(a) + float64(b)) / 2)
+	fmt.Print("\n\n6th task\n", "AverageValue is ", averageValue)
+
+}
